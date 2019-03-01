ad99be82c5a274777849ceaa5cdeb3d5f1cab051
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-03-01 14:35:17 +0200

    WIP

--- lib/bench/factories.rb
+++ lib/bench/factories.rb
@@ -1,35 +1,43 @@
 module Bench
   module Factories
     class << self
-      def create(model, number, options = {})
+      def create(model, options = {})
         "#{self.name}/#{model}"
           .camelize
           .constantize
-          .new(number, options)
-          .create(number, options)
+          .new(options)
+          .create
+      end
+
+      def create_list(model, number, options = {})
+        "#{self.name}/#{model}"
+          .camelize
+          .constantize
+          .new(options)
+          .create_list(number)
       end
     end
 
     class Member
-      def initialize(number, options)
-        @number = number
+      def initialize(options)
         @options = options
       end
 
-      def create(number, options)
-        Array.new(number) do
-          ::Member.create(construct_member)
-        end
+      def create
+        ::Member.create!(construct_member)
+      end
+
+      def create_list(number)
+        Array.new(number) { create }
       end
 
       def construct_member
         { email: unique_email,
           level: 3,
-          disabled: false }
+          disabled: false }.merge(@options)
       end
 
       def unique_email
-        Faker::Internet.unique.email
         @used_emails ||= ::Member.pluck(:email)
         loop do
           email = Faker::Internet.unique.email
@@ -40,5 +48,40 @@ module Bench
         end
       end
     end
+
+    class Deposit
+      DEFAULT_DEPOSIT_AMOUNT = 1_000_000_000
+      def initialize(options)
+        @options = options
+      end
+
+      def create
+        ::Deposit.create!(construct_deposit).tap(&:charge!)
+      end
+
+      def construct_deposit
+        { amount: DEFAULT_DEPOSIT_AMOUNT,
+          type: 'Deposits::Fiat' }.merge(@options)
+      end
+    end
   end
 end
+
+# Table name: deposits
+#
+#  id            :integer          not null, primary key
+#  member_id     :integer          not null
+#  currency_id   :string(10)       not null
+#  amount        :decimal(32, 16)  not null
+#  fee           :decimal(32, 16)  not null
+#  address       :string(64)
+#  txid          :string(128)
+#  txout         :integer
+#  aasm_state    :string(30)       not null
+#  confirmations :integer          default(0), not null
+#  type          :string(30)       not null
+#  tid           :string(64)       not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  completed_at  :datetime
+#
--- lib/bench/matching.rb
+++ lib/bench/matching.rb
@@ -6,11 +6,14 @@ module Bench
     def initialize(config_file_path = 'config/bench/matching.yml')
       @config = YAML.load_file(Rails.root.join(config_file_path)).deep_symbolize_keys
       @injector = Injectors.initialize_injector(@config[:orders])
+      @currencies = Currency.where(id: @config[:currencies].split(',').map(&:squish).reject(&:blank?))
     end
 
     def run!
       Kernel.puts "Creating members ..."
-      @members = Factories.create(:member, @config[:traders])
+      @members = Factories.create_list(:member, @config[:traders])
+      Kernel.puts "Depositing funds ..."
+      @members.map(&method(:become_billionaire))
       Kernel.puts "Generating orders by injector ..."
       @injector.generate!
       run_workers!
@@ -26,6 +29,12 @@ module Bench
           end
         end
       end.map(&:join)
+    end
+
+    def become_billionaire(member)
+      @currencies.each do |c|
+        Factories.create(:deposit, member_id: member.id, currency_id: c.id)
+      end
     end
   end
 end
