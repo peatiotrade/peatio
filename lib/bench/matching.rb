18d232317e8d0c7d0d15201bbee7aadfdde75883
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-03-04 16:25:48 +0200

    Add errors, result. Minor refactor

--- lib/bench/matching.rb
+++ lib/bench/matching.rb
@@ -7,21 +7,41 @@ module Bench
     # TODO: Custom config file support.
     def initialize(config_file_path = 'config/bench/matching.yml')
       @config = YAML.load_file(Rails.root.join(config_file_path)).deep_symbolize_keys
+      @rmq_http_client ||= ::URI::HTTP.build(
+        scheme:   :http,
+        host:     ENV.fetch('RABBITMQ_HOST', 'localhost'),
+        port:     15672,
+        userinfo: "#{ENV.fetch('RABBITMQ_USER', 'guest')}:#{ENV.fetch('RABBITMQ_PASSWORD', 'guest')}"
+      ).yield_self { |endpoint| RabbitMQ::HTTP::Client.new(endpoint.to_s) }
       @injector = Injectors.initialize_injector(@config[:orders])
       @currencies = Currency.where(id: @config[:currencies].split(',').map(&:squish).reject(&:blank?))
+      @errors = []
     end
 
     def run!
       # TODO: Check if Matching daemon is running before run.
       Kernel.puts "Creating members ..."
       @members = Factories.create_list(:member, @config[:traders])
+
       Kernel.puts "Depositing funds ..."
       @members.map(&method(:become_billionaire))
+
       Kernel.puts "Generating orders by injector and saving them in db..."
       @injector.generate!(@members)
+
+      @orders_number = @injector.size
+
       Kernel.puts "Publishing messages to RabbitMQ..."
+      @publish_started_at = Time.now
       publish_messages
-      wait_for_messages_ack
+
+      @publish_finished_at = Time.now
+      Kernel.puts "Messages are published to RabbitMQ. Waiting for processing..."
+      wait_for_messages_processing
+      @consuming_finished_at = Time.now
+
+      Kernel.puts result
+      save_results
     end
 
     def publish_messages
@@ -33,34 +53,43 @@ module Bench
             AMQPQueue.enqueue(:matching, action: 'submit', order: order.to_matching_attributes)
           rescue StandardError => e
             Kernel.puts e
+            @errors << e
           end
         end
       end.map(&:join)
     end
 
-    # Doesn't work correctly.
-    def wait_for_messages_ack
+    def wait_for_messages_processing
       loop do
-        # TODO: Write useful data
-        queue_info.tap do |qi|
-          Kernel.puts "Number of messages in queue is: #{qi[:messages]}"
-          break if qi[:messages].zero?
-        end
         sleep 0.5
+        info = queue_info
+        Kernel.puts "Number of messages in queue is: #{info[:messages]}"
+        break if info[:idle_since].present?
       end
     end
 
-    def queue_info
-      @client ||= ::URI::HTTP.build(
-                    scheme:   :http,
-                    host:     ENV.fetch('RABBITMQ_HOST', 'localhost'),
-                    port:     15672,
-                    userinfo: "#{ENV.fetch('RABBITMQ_USER', 'guest')}:#{ENV.fetch('RABBITMQ_PASSWORD', 'guest')}"
-      ).yield_self { |endpoint| RabbitMQ::HTTP::Client.new(endpoint.to_s) }
+    def result
+      @result ||=
+      begin
+        publish_ops =  @orders_number / (@publish_finished_at - @publish_started_at)
+        consuming_ops =  @orders_number / (@consuming_finished_at - @publish_started_at)
 
-      binding.pry
-      queue_info = @client.queue_info('/', 'peatio.matching').deep_symbolize_keys
-      formatted = queue_info.slice(:messages, :name)
+        {
+          config: @config,
+          publish_started_at: @publish_started_at,
+          publish_finished_at: @publish_finished_at,
+          consuming_finished_at: @consuming_finished_at,
+          publish_ops: publish_ops,
+          consuming_ops: consuming_ops
+        }
+      end
+    end
+
+    def save_results
+    end
+
+    def queue_info
+      @rmq_http_client.queue_info('/', AMQPConfig.binding_queue(:matching).first).deep_symbolize_keys
     end
 
     private
