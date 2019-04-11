b88867373171b959dd6bb39f914f323da9a05b27
mnaichuk <mnaichuk@heliostech.fr>
2019-04-11 18:22:59 +0300

    FIRST WORKING SPEC111111111

--- spec/services/blockchain_service2_spec.rb
+++ spec/services/blockchain_service2_spec.rb
@@ -1,12 +1,37 @@
 # encoding: UTF-8
 # frozen_string_literal: true
 
-FakeBlockchain = Class.new(Peatio::Blockchain::Abstract)
+FakeBlockchain = Peatio::Blockchain::Abstract.new
 Peatio::BlockchainAPI.register(:fake, FakeBlockchain)
 
 describe BlockchainService2 do
-  # TODO: Create blockchain with fake client.
-  # TODO: Create 2 currencies for blockchain with fake client.
+  let!(:blockchain) { Blockchain.create!(key: 'fake', name: 'fake', client: 'fake', status: 'active', height: 1) }
+  let(:service) { BlockchainService2.new(blockchain) }
+  let!(:fake_currency) { Currency.create!(id: 'fake', name: 'fake', blockchain: blockchain, symbol: 'F') }
+  let!(:fake_currency1) { Currency.create!(id: 'fake1', name: 'fake1', blockchain: blockchain, symbol: 'G') }
+  let!(:member) { create(:member) }
+  let(:block_number) { 1 }
+  let(:transaction) {  Peatio::Transaction.new(hash: 'fake_hash', from_address: 'fake_address', to_address: 'fake_address', amount: 1, block_number: 2, currency_id: 'fake') }
+  let(:expected_transactions) do 
+    [
+      Peatio::Transaction.new(hash: 'fake_hash', from_address: 'fake_address', to_address: 'fake_address', amount: 1, block_number: 2, currency_id: 'fake', txout: 1),
+      Peatio::Transaction.new(hash: 'fake_hash', from_address: 'fake_address', to_address: 'fake_address', amount: 2, block_number: 2, currency_id: 'fake', txout: 2),
+      Peatio::Transaction.new(hash: 'fake_hash', from_address: 'fake_address', to_address: 'fake_address', amount: 3, block_number: 2, currency_id: 'fake', txout: 3)
+    ]
+  end
+  it do
+    service.adapter.stubs(:fetch_block!).returns(expected_transactions)
+    PaymentAddress.stubs(:where).returns([{ address: 'fake_address' }])
+    PaymentAddress.stubs(:find_by).returns(member.accounts.first.payment_address)
+    Deposits::Coin.any_instance.stubs(:blockchain_api).returns(FakeBlockchain)
+    FakeBlockchain.class.any_instance.stubs(:case_sensitive?).returns(true)
+    FakeBlockchain.class.any_instance.stubs(:supports_cash_addr_format?).returns(false)
+    Blockchain.any_instance.stubs(:blockchain_api).returns(FakeBlockchain)
+    FakeBlockchain.class.any_instance.stubs(:latest_block_number).returns(4)
+    # allow(BlockchainClient::Fake).to recieve(:case_sensetive).and_return(false)
+    service.process_block(1)
+    expect(Deposit.count).to eq 3
+  end
   # Deposit context: (mock fetch_block)
   #   * Single deposit in block which should be saved.
   #   * Multiple deposits in single block (one saved one updated).
